1c219fdf31af12c111088c6d9c6a34c1d03ed4a2
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-20 21:03:17 +0300

    Classes contest

---
+++ src/0001-1000/0925.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+struct Point {
+    double x, y;
+};
+
+enum TriangleType {
+    TriangleTypeImpossible,
+    TriangleTypeRight,
+    TriangleTypeObtuse,
+    TriangleTypeAcute,
+};
+
+class Triangle {
+private:
+    std::vector<double> sides;
+public:
+    explicit Triangle(std::vector<double> sides);
+
+    Triangle(Point p0, Point p1, Point p2);
+
+    TriangleType type();
+
+    double perimeter();
+
+    double area();
+
+    std::vector<double> heights();
+};
+
+Triangle::Triangle(std::vector<double> sides) {
+    this->sides = std::move(sides);
+}
+
+Triangle::Triangle(Point p0, Point p1, Point p2) {
+    // calculate sides
+
+    double a = sqrt((p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y) * (p1.y - p0.y));
+    double b = sqrt((p2.x - p0.x) * (p2.x - p0.x) + (p2.y - p0.y) * (p2.y - p0.y));
+    double c = sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+    sides = std::vector(3, 0.0);
+    sides[0] = a;
+    sides[1] = b;
+    sides[2] = c;
+    std::sort(sides.begin(), sides.end());
+}
+
+TriangleType Triangle::type() {
+    std::vector<double> s = this->sides;
+    std::sort(s.begin(), s.end());
+
+    if (s[2] >= s[1] + s[0]) {
+        return TriangleTypeImpossible;
+    } else {
+        double c = s[2] * s[2] - s[1] * s[1] - s[0] * s[0];
+        if (c == 0)
+            return TriangleTypeRight;
+        else if (c > 0)
+            return TriangleTypeObtuse;
+        else
+            return TriangleTypeAcute;
+    }
+}
+
+double Triangle::perimeter() {
+    return std::accumulate(sides.begin(), sides.end(), 0.0);
+}
+
+double Triangle::area() {
+    double hp = perimeter() / 2;
+    return sqrt(hp * (hp - sides[0]) * (hp - sides[1]) * (hp - sides[2]));
+}
+
+std::vector<double> Triangle::heights() {
+    return {
+            (2/sides[0])*area(),
+            (2/sides[1])*area(),
+            (2/sides[2])*area()
+    };
+}
+
+int main() {
+    std::vector<Point> a(3);
+
+    for (auto &l: a) {
+        std::cin >> l.x >> l.y;
+    }
+    Triangle t(a[0], a[1], a[2]);
+
+    printf("%.4lf %.4lf\n", t.perimeter(), t.area());
+
+    return 0;
+}
